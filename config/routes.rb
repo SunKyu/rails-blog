5817686bdab4739fbdba2e1c90d5c4f8b0e5cb90
SunKyu <leesunkeuy@naver.com>
2016-03-05 14:48:44 +0900

    make root index file
    and use home controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   ActiveAdmin.routes(self)
   resources :posts
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
